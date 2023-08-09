45cffed87f20a912133e666d2d2aa2aa9bcffa86
Vlad <vladbolkunovv@gmail.com>
2023-08-09 23:54:42 +0300

    fix(use-body): todo notice

--- src/middlewares/use-body.ts
+++ src/middlewares/use-body.ts
@@ -2,7 +2,7 @@ import { RMiddleware } from '../route';
 
 type Class<C> = { new (): C };
 
-// TODO class-validation (~& joi)
+// TODO class-validation (~& joi) + swagger annotation
 export function useBody<C>(schema?: Class<C>): RMiddleware<any, { body: C }> {
   return (ctx, kctx) => {
     return { body: kctx.request.body as C };
