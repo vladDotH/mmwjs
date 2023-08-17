7808e77d26157449e625522ec8a00e21f973862e
Vlad <vladbolkunovv@gmail.com>
2023-08-18 00:19:05 +0300

    feat: useAsyncBody

--- src/middlewares/use-body.ts
+++ src/middlewares/use-body.ts
@@ -7,21 +7,27 @@ import Joi from 'joi';
 type Class<C> = { new (): C };
 
 // TODO swagger annotation
-export function useBody<C extends object>(
+
+export function useAsyncBody<C extends object>(
   schema?: Class<C> | Joi.ObjectSchema<C>,
-): RMiddleware<any, { body: C }> {
-  return async (ctx, kctx) => {
+): RMiddleware<any, { body: Promise<C> }> {
+  return (ctx, kctx) => {
     const rawBody = kctx.request.body as object;
 
     // Joi validation
-    if ('validate' in schema) {
-      const res = schema.validate(rawBody);
-
-      if (res.error) {
-        throw createHttpError(400, res.error);
-      }
-
-      return { body: res.value };
+    if ('validateAsync' in schema) {
+      const res = schema.validateAsync(rawBody);
+      return {
+        body: new Promise<C>((resolve, reject) => {
+          res
+            .then((value) => {
+              resolve(value);
+            })
+            .catch((err) => {
+              reject(createHttpError(400, err));
+            });
+        }),
+      };
     }
     // Class-validator validation
     else if (isFunction(schema)) {
@@ -30,15 +36,29 @@ export function useBody<C extends object>(
       for (const key in rawBody) {
         body[key] = rawBody[key];
       }
-      const res = await validate(body);
+      const res = validate(body);
 
-      if (res.length) {
-        throw createHttpError(400, JSON.stringify(res));
-      }
-
-      return { body: rawBody as C };
+      return {
+        body: new Promise<C>((resolve, reject) => {
+          res.then((value) => {
+            if (value.length) {
+              reject(createHttpError(400, JSON.stringify(value)));
+            } else resolve(rawBody as C);
+          });
+        }),
+      };
     }
 
-    return { body: kctx.request.body as C };
+    return { body: Promise.resolve(rawBody as C) };
+  };
+}
+
+export function useBody<C extends object>(
+  schema?: Class<C> | Joi.ObjectSchema<C>,
+): RMiddleware<any, { body: C }> {
+  const bodyFn = useAsyncBody(schema);
+  return async (ctx, kctx) => {
+    const res = bodyFn(ctx, kctx);
+    return { body: await (await res).body };
   };
 }
