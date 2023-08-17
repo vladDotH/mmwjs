59db177ed63fb2feb3a11cd1dac327ca35ee29f2
Vlad <vladbolkunovv@gmail.com>
2023-08-18 00:29:20 +0300

    refactor(useBody): joi & class MW validator composition

--- src/middlewares/use-body.ts
+++ src/middlewares/use-body.ts
@@ -8,48 +8,61 @@ type Class<C> = { new (): C };
 
 // TODO swagger annotation
 
+function useAsyncJoiBody<C extends object>(
+  schema: Joi.ObjectSchema<C>,
+): RMiddleware<any, { body: Promise<C> }> {
+  return (ctx, kctx) => {
+    const res = schema.validateAsync(kctx.request.body);
+    return {
+      body: new Promise<C>((resolve, reject) => {
+        res
+          .then((value) => {
+            resolve(value);
+          })
+          .catch((err) => {
+            reject(createHttpError(400, err));
+          });
+      }),
+    };
+  };
+}
+
+function useAsyncClassBody<C extends object>(
+  schema: Class<C>,
+): RMiddleware<any, { body: Promise<C> }> {
+  return (ctx, kctx) => {
+    const rawBody = kctx.request.body as object,
+      body = new schema();
+    for (const key in rawBody) {
+      body[key] = rawBody[key];
+    }
+    const res = validate(body);
+    return {
+      body: new Promise<C>((resolve, reject) => {
+        res.then((value) => {
+          if (value.length) {
+            reject(createHttpError(400, JSON.stringify(value)));
+          } else resolve(rawBody as C);
+        });
+      }),
+    };
+  };
+}
+
 export function useAsyncBody<C extends object>(
   schema?: Class<C> | Joi.ObjectSchema<C>,
 ): RMiddleware<any, { body: Promise<C> }> {
+  // Joi validator
+  if ('validateAsync' in schema) {
+    return useAsyncJoiBody(schema);
+  }
+  // Class-validator
+  else if (isFunction(schema)) {
+    return useAsyncClassBody(schema);
+  }
+  // Raw body
   return (ctx, kctx) => {
-    const rawBody = kctx.request.body as object;
-
-    // Joi validation
-    if ('validateAsync' in schema) {
-      const res = schema.validateAsync(rawBody);
-      return {
-        body: new Promise<C>((resolve, reject) => {
-          res
-            .then((value) => {
-              resolve(value);
-            })
-            .catch((err) => {
-              reject(createHttpError(400, err));
-            });
-        }),
-      };
-    }
-    // Class-validator validation
-    else if (isFunction(schema)) {
-      const body = new schema();
-
-      for (const key in rawBody) {
-        body[key] = rawBody[key];
-      }
-      const res = validate(body);
-
-      return {
-        body: new Promise<C>((resolve, reject) => {
-          res.then((value) => {
-            if (value.length) {
-              reject(createHttpError(400, JSON.stringify(value)));
-            } else resolve(rawBody as C);
-          });
-        }),
-      };
-    }
-
-    return { body: Promise.resolve(rawBody as C) };
+    return { body: Promise.resolve(kctx.request.body as C) };
   };
 }
 
