9ad4b7a21676d05269f5895983ffd7fdf93e7c49
Vlad <vladbolkunovv@gmail.com>
2024-01-10 16:28:17 +0300

    fix(await-service): correct ctx return

--- src/middlewares/await-service.ts
+++ src/middlewares/await-service.ts
@@ -1,9 +1,11 @@
 import { RMiddleware } from '../route';
 import { AsyncService } from '../service';
 
-export function awaitService<S, T>(srv: AsyncService<S>): RMiddleware<T, T> {
+export function awaitService<S, T>(
+  srv: AsyncService<S>,
+): RMiddleware<T, unknown> {
   return async (ctx) => {
     if (srv.status === 'pending') await srv.setup;
-    return ctx;
+    return {};
   };
 }
