e2397b82cb3fdfcfc23bbce9e1f5553ce89d18f9
Vlad <vladbolkunovv@gmail.com>
2024-01-11 14:34:58 +0300

    refactor: core types

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,9 @@ export type Rewrite<T, V> = Omit<T, keyof V> & V;
 export type Save<T, V> = Rewrite<V, T>;
 export type PromiseOr<V> = V | Promise<V>;
 
+export type Fn<I = any, O = any> = (value: I) => O;
+export type ClassSchema<C> = { new (): C };
+
 export enum RType {
   GET = 'get',
   POST = 'post',
