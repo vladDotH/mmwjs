ca514b25bc7510b66849fe74b7058850d6e33d9e
Vlad <vladbolkunovv@gmail.com>
2024-01-12 21:50:28 +0300

    fix(await-service): unused fn param

--- src/middlewares/await-service.ts
+++ src/middlewares/await-service.ts
@@ -4,7 +4,7 @@ import { AsyncService } from '../service';
 export function awaitService<S, T>(
   srv: AsyncService<S>,
 ): RMiddleware<T, unknown> {
-  return async (ctx) => {
+  return async () => {
     if (srv.status === 'pending') await srv.setup;
     return {};
   };
